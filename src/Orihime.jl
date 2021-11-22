67aeba799b0a982392377ab4de59f8a819e49cd5
Takafumi Arakaki <aka.tkf@gmail.com>
2021-11-22 16:06:30 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/Orihime.jl
@@ -0,0 +1,11 @@
+baremodule Orihime
+
+module Internal
+
+using ..Orihime: Orihime
+
+include("internal.jl")
+
+end  # module Internal
+
+end  # baremodule Orihime
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using Orihime
+using Test
+
+@testset "Orihime.jl" begin
+    # Write your tests here.
+end
